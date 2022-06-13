d20a377c33295755e0e9b5a5216cd1fc80cd65ef
GitHub Action <action@github.com>
2022-06-13 12:53:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/lgouger/cyclespacing/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lgouger/cyclespacing/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lgouger.cyclespacing.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lgouger.cyclespacing.MyBundle
 
 class MyApplicationService {
 
